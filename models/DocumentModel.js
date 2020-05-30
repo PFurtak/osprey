ee3f81b391fcdbcde26065d574afe67dfe863204
Patrick Furtak <patrick@patrickfurtak.com>
2020-05-30 17:17:00 -0400

    Created document model

---
+++ models/DocumentModel.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const DocumentSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  deviceSerialNumber: {
+    type: String,
+  },
+  deviceType: {
+    type: String,
+    required: true,
+  },
+  deviceIP: {
+    type: String,
+  },
+  deviceLocation: {
+    type: String,
+  },
+  licenseStart: {
+    type: Date,
+  },
+  licenseExpire: {
+    type: Date,
+  },
+  notes: {
+    type: String,
+  },
+  createdBy: {
+    type: String,
+  },
+  createDate: {
+    type: Date.now,
+  },
+});
+
+module.exports = mongoose.model('document', DocumentSchema);
