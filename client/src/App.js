437881e802c6856c90fbfe21690c8206e9773c27
Patrick Furtak <patrick@patrickfurtak.com>
2020-10-27 17:47:16 -0400

    Created header component

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,24 @@
-import { ThemeProvider } from '@chakra-ui/core';
-import { Button } from '@chakra-ui/core';
+import React from 'react';
+import Header from './components/layout/Header';
+import { theme, ThemeProvider, CSSReset } from '@chakra-ui/core';
+
+const breakpoints = ['160px', '768px', '1024px', '1440px'];
+breakpoints.sm = breakpoints[0];
+breakpoints.md = breakpoints[1];
+breakpoints.lg = breakpoints[2];
+breakpoints.xl = breakpoints[3];
+
+const newTheme = {
+  ...theme,
+  breakpoints,
+};
 
 function App() {
   return (
-    <div className='App'>
-      <ThemeProvider>
-        <h1>Hello World</h1>
-        <Button>Test</Button>
-      </ThemeProvider>
-    </div>
+    <ThemeProvider theme={newTheme}>
+      <CSSReset />
+      <Header />
+    </ThemeProvider>
   );
 }
 
